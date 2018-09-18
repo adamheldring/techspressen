4ca8da21fe231dc438869fc5132f9635fd7a52a8
charlotteflygare <38113960+charlotteflygare@users.noreply.github.com>
2018-09-18 17:03:35 +0200

    Api

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // This is the js file
+var url = 'https://newsapi.org/v2/everything?' +
+          'q=space&' +
+          'apiKey=3934b18b3b584fcdbdfbae1b25021f3a';
+var req = new Request(url);
+fetch(req)
+    .then(function(response) {
+        console.log(response.json());
+    })
