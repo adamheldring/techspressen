2fdbeeef1dae3afe58e68a6edacd2fc6bce70464
charlotteflygare <38113960+charlotteflygare@users.noreply.github.com>
2018-09-19 14:18:22 +0200

    Javascript

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ fetch(req)
     .then(function(response) {
         console.log(response.json());
     })
+
+    const expand = () => {
+      this.classList.toggle('expanded')
+      console.log(this.classList)
+    }
+
+document.getElementsByClassName('gridArticle big')[0].onclick = expand
+document.getElementsByClassName('gridArticle')[1].onclick = expand
+document.getElementsByClassName('gridArticle')[2].onclick = expand
+document.getElementsByClassName('gridArticle')[3].onclick = expand
+document.getElementsByClassName('gridArticle')[4].onclick = expand
+document.getElementsByClassName('gridArticle')[5].onclick = expand
+document.getElementsByClassName('gridArticle')[6].onclick = expand
+document.getElementsByClassName('gridArticle')[7].onclick = expand
+document.getElementsByClassName('gridArticle')[8].onclick = expand
+document.getElementsByClassName('gridArticle')[9].onclick = expand
+document.getElementsByClassName('gridArticle')[10].onclick = expand
+document.getElementsByClassName('gridArticle')[11].onclick = expand
+document.getElementsByClassName('gridArticle')[12].onclick = expand
+document.getElementsByClassName('gridArticle')[13].onclick = expand
+document.getElementsByClassName('gridArticle')[14].onclick = expand
