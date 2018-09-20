7e8d440a79e67d66311f888bcf4374fd20179fe7
A-Game <javascript@adamheldring.com>
2018-09-20 23:04:01 +0200

    Creating elements with JS on the fly

--- script.js
+++ script.js
@@ -22,14 +22,35 @@ fetch(req)
       }
 
 
-      // Creating element on the fly
+      // CREATING ELEMENTS ON THE FLY
 
-      // const grid = document.getElementsByClassName('gridContainer')
-      // grid.style.background = "black"
-      // let newDiv = document.createElement('div')
-      // newDiv.style.background = "yellow"
-      // grid.appendChild(newDiv)
+      const grid = document.getElementById('grid')
 
+      //Change 15 to whatever is appropriate and use a variable, also make sure that the CSS grid contains proper amount of rows!!
+      for (i = 0; i < 15; i++) {
+        const newArticle = document.createElement('div')    //Creates new article div
+        newArticle.className = "gridArticle small"          //Adds classes to article div
+        newArticle.style.background = "grey"                //TEST makes background grey for JS on the fly made article divs
+
+        const newTitleBox = document.createElement('div')   //Created new title container
+        newTitleBox.className = "articleTitle"              //Adds classes to title div
+
+
+        //Probably best to make title some sort of header instead
+        const newTitle = document.createTextNode(result.articles[i].title)  //TEST writes text in div
+        newTitleBox.appendChild(newTitle)                     //TEST appends text to new div
+
+        newArticle.appendChild(newTitleBox)                 //Adds title container to article div
+
+
+        const newImageBox = document.createElement('div')   //Creates new image container
+        newImageBox.className = "articleImage"              //Adds classes image div
+        newImageBox.style.backgroundImage = `url('${result.articles[i].urlToImage}')`;
+        newArticle.appendChild(newImageBox)                 //Adds image container to article div
+
+
+        grid.appendChild(newArticle)                        //Appends new article to grid
+      }
 
 
     })
