386aed3f3dae11be7ebdc377dae5b205cb0bc790
A-Game <javascript@adamheldring.com>
2018-09-20 22:20:31 +0200

    revert catch

--- script.js
+++ script.js
@@ -11,14 +11,26 @@ fetch(req)
         return response.json();
     }).then(function(result){
         console.log(result.articles[0]);
-    }).catch(error => {
-        console.log(error)
+    // }).catch(error => {
+    //     console.log(error)
 
       for(i = 0; i < 15; i++) {
         console.log(i);
         document.getElementsByClassName('articleTitle')[i].innerHTML = result.articles[i].title;
         document.getElementsByClassName('articleImage')[i].style.backgroundImage = "url('" + result.articles[i].urlToImage + "')";
+
       }
+
+
+      // Creating element on the fly
+
+      // const grid = document.getElementsByClassName('gridContainer')
+      // grid.style.background = "black"
+      // let newDiv = document.createElement('div')
+      // newDiv.style.background = "yellow"
+      // grid.appendChild(newDiv)
+
+
 
     })
 
